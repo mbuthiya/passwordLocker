a1c7b07236a5191e768724f79f4b4d4cb6f548fa
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-08 22:33:26 +0300

    Create a function to check account Created a function to check if there is an account with the parameter passed and retrieve the password

--- pw.py
+++ pw.py
@@ -1,6 +1,6 @@
 #!  /usr/bin/python3.6
 # A Password locker program that allows you to generate new passwords and store your own
-import string,random,sys
+import string,random,sys,pyperclip
 
 # function to generate password  and return it
 def generate_password():
@@ -13,6 +13,30 @@ def generate_password():
         new_password = new_password + letters_list[random_integer] + str(number_list[random_integer])
 
     return new_password
-# Function to store new accounts and passwords
-def store_password(new_account, new_password):
-    passwords[new_account] = new_password
+
+def check_account(account_name):
+    if account_name in passwords:
+        pyperclip.copy(passwords[account_name])
+        print(f"The {account_name} password has been coppied to clipboard")
+    else:
+        print('There is no account with that name stored would you like to generate a new password y/n:')
+        response = input()
+
+        if response == 'y':
+            pyperclip.copy(generate_password())
+            print(f"The {account_name} password has been coppied to clipboard")
+        else:
+            sys.exit()
+
+
+
+
+# Dictionary to store the passwords and accounts
+passwords = {'email':'2kdknknjndj'}
+#Checking if there is a second argument passed in the command line
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+check_account(account)
