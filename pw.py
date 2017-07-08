68554afd4c37f49911c4dba3a14b1356f98a40cf
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-08 22:11:27 +0300

    Create function to generate password Created a function that could generate new passwords for accounts

--- pw.py
+++ pw.py
@@ -1,9 +1,15 @@
 #!  /usr/bin/python3.6
 # A Password locker program that allows you to generate new passwords and store your own
-import string,random
+import string,random,sys
 
-letters_list =list(string.ascii_letters)
-number_list = list(range(0,53))
+# function to generate password  and return it
+def generate_password():
+    letters_list =list(string.ascii_letters)
+    number_list = list(range(0,53))
+    new_password =''
 
-for i in range(0,11):
-    
+    for i in range(0,4):
+        random_integer = random.randint(0,52)
+        new_password = new_password + letters_list[random_integer] + str(number_list[random_integer])
+
+    return new_password
